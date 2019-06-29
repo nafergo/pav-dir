1c1e39ce5c86eb816210258a2eae03ca621f1318
nelson gonçalves <nafergo@gmail.com>
2019-06-29 21:12:23 +0100

    Update isotopeinit.js

--- js/isotopeinit.js
+++ js/isotopeinit.js
@@ -1,29 +1,9 @@
 // init Isotope
-var $table = $('.table-like').isotope({
-  layoutMode: 'vertical',
-  getSortData: {
-    name: '.name',
-    symbol: '.symbol',
-    number: '.number parseInt',
-    category: '.category',
-    weight: function( itemElem ) {
-      var weight = $( itemElem ).find('.weight').text();
-      return parseFloat( weight.replace( /[\(\)]/g, '') );
-    }
-  }
+var $grid = $('.grid').isotope({
+  // options
 });
-
-// bind sort button click
-$('#sorts').on( 'click', 'button', function() {
-  var sortValue = $(this).attr('data-sort-value');
-  $table.isotope({ sortBy: sortValue });
-});
-
-// change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
-  });
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
 });
