2ecb8f1959d3f8a591c3a1ec67ab9d06091aef3a
nafergo <nafergo@gmail.com>
2019-07-01 15:33:59 +0100

    0

--- old2/site.js
+++ old2/site.js
@@ -26,27 +26,10 @@ function init() {
 
 
 
-//Get Params in URL
-//function getParameterByName(name, url) {
-//    if (!url) url = window.location.href;
-//    name = name.replace(/[\[\]]/g, "\\$&").toLowerCase(); // This is just to avoid case sensitiveness for query parameter name
-//    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-//        results = regex.exec(url);
-//    if (!results) return null;
-//    if (!results[2]) return '';
-//    return decodeURIComponent(results[2].replace(/\+/g, " "));
-//}
-
-params.spreadsheetKey = '1zcf8HFaI2WesW-NkawO0g6pSrg4B8R5uH1l8oMI0K-I';
-params.pageTitle = 'teste';
-params.primaryColor = getParameterByName('primary-color');
-params.secondaryColor = getParameterByName('secondary-color');
-
 var getList = {
   tabletop: function() {
     Tabletop.init({
       key: '1zcf8HFaI2WesW-NkawO0g6pSrg4B8R5uH1l8oMI0K-I', // copy of live spreadsheet
-      //key: params.spreadsheetKey, // copy of live spreadsheet
       callback: success
     });
   },
@@ -60,7 +43,7 @@ var getList = {
       }
     });
   }
-};
+}
 
 /*
 callback function after the list data has
