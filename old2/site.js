54b15c9ff73ca02e3d356e08f344eab91631dbcb
nelson gonçalves <nafergo@gmail.com>
2019-07-01 15:29:15 +0100

    Update site.js

--- old2/site.js
+++ old2/site.js
@@ -27,15 +27,15 @@ function init() {
 
 
 //Get Params in URL
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&").toLowerCase(); // This is just to avoid case sensitiveness for query parameter name
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
+//function getParameterByName(name, url) {
+//    if (!url) url = window.location.href;
+//    name = name.replace(/[\[\]]/g, "\\$&").toLowerCase(); // This is just to avoid case sensitiveness for query parameter name
+//    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+//        results = regex.exec(url);
+//    if (!results) return null;
+//    if (!results[2]) return '';
+//    return decodeURIComponent(results[2].replace(/\+/g, " "));
+//}
 
 params.spreadsheetKey = '1zcf8HFaI2WesW-NkawO0g6pSrg4B8R5uH1l8oMI0K-I';
 params.pageTitle = 'teste';
