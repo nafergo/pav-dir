60156e07ffa3997be735e777be22da280580c3e4
nelson gonçalves <nafergo@gmail.com>
2019-07-05 14:12:07 +0100

    Update site.js

--- static/site.js
+++ static/site.js
@@ -25,22 +25,22 @@ function init() {
 }
 
 //Determine device type for iOS or Android - used for mapping application link.
-function getMobileOperatingSystem() {
-  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-  if( userAgent.match( /iPad/i ) || userAgent.match( /iPhone/i ) || userAgent.match( /iPod/i ) )
-  {
-    return 'iOS';
-  }
-  else if( userAgent.match( /Android/i ) )
-  {
-    return 'Android';
-  }
-  else
-  {
-    return 'unknown';
-  }
-}
+//function getMobileOperatingSystem() {
+//  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+//
+//  if( userAgent.match( /iPad/i ) || userAgent.match( /iPhone/i ) || userAgent.match( /iPod/i ) )
+//  {
+//    return 'iOS';
+//  }
+//  else if( userAgent.match( /Android/i ) )
+//  {
+//    return 'Android';
+//  }
+//  else
+//  {
+//    return 'unknown';
+//}
+//
 
 //Get Params in URL
 function getParameterByName(name, url) {
